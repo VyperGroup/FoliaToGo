51e2199a0fcb47d5e95a8be0b42bbfa5e5b7c66c
Soundarahari Parthiban <66728880+FatGrizzly@users.noreply.github.com>
2024-06-04 13:21:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ git clone https://github.com/PaperMC/Folia
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
+rm -rf gradle.properties
+wget https://raw.githubusercontent.com/VyperGroup/FoliaToGo/main/gradle.properties
 ./gradlew applyPatches
 ./gradlew createReobfPaperclipJar
